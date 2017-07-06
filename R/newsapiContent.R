dfdbe7b470c45f4afa3001e5c108108b2df19b20
rmnppt <romanpopat@gmx.com>
2017-07-06 14:36:12 +0100

    changed default removal of stopwords in newsapiContent, also changes the name to newsContent

--- R/newsapiContent.R
+++ R/newsapiContent.R
@@ -1,11 +1,12 @@
-#' newsapiArticles
+#' newsContent
 #'
 #' A function to retrieve articles from newsapi.org
 #'
 #' @param url (required) the url of the page from which to get the content
 #' @param tag (required) the html tag from which to get the content
 #'                       e.g. "p" passed to \code{\link[xml2]{read_html}}.
-#'
+#' @param removeStop (logical) remove stopwords from the raw text with an antijoin to
+#'                   tidytext::stopwords? defaults to "FALSE".
 #' @keywords news, api
 #'
 #' @import tidytext
@@ -13,7 +14,7 @@
 #' @import dplyr
 #' @import xml2
 #' @export
-newsapiContent <- function(url, tag) {
+newsContent <- function(url, tag, removeStop = "FALSE") {
   page <- xml2::read_html(url)
   text <- page %>%
     rvest::html_nodes(tag) %>%
@@ -21,7 +22,7 @@ newsapiContent <- function(url, tag) {
     as.data.frame
   names(text) <- "text"
   text$text <- as.character(text$text)
-  token <- tidytext::unnest_tokens(text, word, text) %>%
-    dplyr::anti_join(tidytext::stop_words, by = "word")
+  token <- tidytext::unnest_tokens(text, word, text)
+  if(removeStop) token <- dplyr::anti_join(tidytext::stop_words, by = "word")
   return(token$word)
 }
